1d24c5a1682519cf986c7729a6b1d3778137a44b
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-25 19:55:32 +0530

    removed log statements

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,10 +1,7 @@
 export default function decorate(block) {
-    // console.log(block);
     Array.from(block.children).forEach(function(ele) {
-        // console.log(ele);
         Array.from(ele.children).forEach(function(childEle) {
-            // console.log(childEle);
             childEle.classList.add('full-width');
-        })
-    })
+        });
+    });
 }
