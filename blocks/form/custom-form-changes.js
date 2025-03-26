48db0c5121fb1cd19ba5cdc1ec5832eccae5edd2
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-26 13:55:24 +0530

    added wrapper for form

---
+++ blocks/form/custom-form-changes.js
@@ -0,0 +1,24 @@
+export default function decorateCustomForm(block) {
+
+  const formEle = document.querySelector('form');
+  console.log('formEle', formEle);
+  formEle.setAttribute('class', 'form-element');
+
+  const personFieldWrapper = document.querySelector('.personName.field-wrapper.text-wrapper');
+  const mobileFieldWrapper = document.querySelector('.mobileNumber.field-wrapper.text-wrapper');
+  const pincodeFieldWrapper = document.querySelector('.pinCode.field-wrapper.text-wrapper');
+
+  const textFieldParentWrapper = document.createElement('div');
+  textFieldParentWrapper.classList.add('text-field-parent-wrapper');
+
+  textFieldParentWrapper.appendChild(personFieldWrapper);
+  textFieldParentWrapper.appendChild(mobileFieldWrapper);
+  textFieldParentWrapper.appendChild(pincodeFieldWrapper);
+
+  const checkboxWrapper = document.querySelector('.checkbox-wrapper');
+  const formWrapper = document.querySelector('.form-element');
+  
+  formWrapper.insertBefore(textFieldParentWrapper, checkboxWrapper);
+
+  return formWrapper;
+}
