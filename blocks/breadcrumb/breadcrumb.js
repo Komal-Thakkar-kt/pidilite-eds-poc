fb266975ecada8489e0549b1a8d672d67b079aef
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-27 15:32:48 +0530

    lint fixes 8

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,5 +1,5 @@
 export default function decorateBreadcrumb() {
-	const sourceWrapper = document.querySelector('.nav-wrapper');
+const sourceWrapper = document.querySelector('.nav-wrapper');
 	const childElement = document.querySelector('.breadcrumbs');
 	const removedChild = sourceWrapper.removeChild(childElement);
 	const destinationWrapper = document.querySelector('.hero-wrapper');
