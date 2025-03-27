5b142fa1941d60abbe64af317d95920ae5b931c3
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-27 12:07:35 +0530

    indentation change

--- blocks/breadcrumb/breadcrumb.js
+++ blocks/breadcrumb/breadcrumb.js
@@ -1,7 +1,7 @@
 export default function decorateBreadcrumb(block) {
-    const sourceWrapper = document.querySelector('.nav-wrapper');
-    const childElement = document.querySelector('.breadcrumbs');
-    const removedChild = sourceWrapper.removeChild(childElement);
-    const destinationWrapper = document.querySelector('.hero-wrapper');
-    destinationWrapper.appendChild(removedChild);
+	const sourceWrapper = document.querySelector('.nav-wrapper');
+	const childElement = document.querySelector('.breadcrumbs');
+	const removedChild = sourceWrapper.removeChild(childElement);
+	const destinationWrapper = document.querySelector('.hero-wrapper');
+	destinationWrapper.appendChild(removedChild);
 }
