468d13914ac4d55e8b936802cb9f470b7fc8f13d
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-27 13:18:02 +0530

    lint fixes 3

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,8 +1,8 @@
 export default function decorateTeaser(block) {
-  Array.from(block.children).forEach(function (ele) {
+  Array.from(block.children).forEach((ele) => {
     ele.classList.add('teaser-content');
     const childEle = document.querySelectorAll('.teaser-content');
-    Array.from(childEle).forEach(function (element) {
+    Array.from(childEle).forEach((element) => {
       element.children[0].classList.add('teaser-content-left');
       element.children[1].classList.add('teaser-content-right');
     });
