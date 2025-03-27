14dbc4b0bdcde865c3c59bdaa99990884e438ca5
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-27 13:14:22 +0530

    lint fixes 2

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -2,7 +2,7 @@ export default function decorateText(block) {
   Array.from(block.children).forEach((ele) => {
     ele.classList.add('text-content');
     const childEle = document.querySelectorAll('.text-content');
-    Array.from(childEle).forEach(function (element) {
+    Array.from(childEle).forEach((element) => {
       element.children[0].classList.add('text-content-wrapper');
     });
   });
