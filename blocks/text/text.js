45314ab1c27aba9430f238ed186bbc6f0e42217e
Komal Thakkar <komal.thakkar@teknopoint.local>
2025-03-27 13:03:37 +0530

    indentation change 2

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -2,8 +2,8 @@ export default function decorateText(block) {
   Array.from(block.children).forEach(function (ele) {
     ele.classList.add('text-content');
     const childEle = document.querySelectorAll('.text-content');
-    Array.from(childEle).forEach(function (ele) {
-      ele.children[0].classList.add('text-content-wrapper');
+    Array.from(childEle).forEach(function (element) {
+      element.children[0].classList.add('text-content-wrapper');
     });
   });
 }
