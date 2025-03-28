291f3925758a2ae0279ce39b881513f7efc63b4f
yashteli <yash.teli@teknopoint.in>
2025-03-28 15:49:05 +0530

    header mobileview css done & icon added

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -241,3 +241,13 @@ export default async function decorate(block) {
     navWrapper.append(await buildBreadcrumbs());
   }
 }
+
+const bodyElements = document.getElementsByTagName('body');
+const body = bodyElements[0];
+window.addEventListener('scroll', function(){
+  if (window.scrollY > 0) {
+    body.classList.add('scrolled');
+  }else{
+    body.classList.remove('scrolled');
+  }
+});
