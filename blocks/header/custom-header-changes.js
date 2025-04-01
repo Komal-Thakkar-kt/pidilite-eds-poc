41d40e87d2cc4a98374a15eb59acc7032c79f58e
yashteli <yash.teli@teknopoint.in>
2025-04-01 19:29:40 +0530

    Fix:Custom header js and css with breadcrumb code added

--- blocks/header/custom-header-changes.js
+++ blocks/header/custom-header-changes.js
@@ -1,9 +1,30 @@
-export default function decorateSearch() {
-  const searchWrapper = document.querySelector('.nav-sections .default-content-wrapper p');
-  const inputElement = document.createElement('input');
-  inputElement.setAttribute('type', 'text');
-  inputElement.setAttribute('placeholder', 'SEARCH');
-  searchWrapper.appendChild(inputElement);
+export default async function decorateSearch() {
+
+  const isMobile = () => window.matchMedia("(max-width: 767px)").matches;
+  if (isMobile()) {
+    const navWrapper = document.querySelector('.nav-wrapper');
+    const navSearchInputHTML = `
+    <div class="search-input-wrapper">
+      <img src="../../icons/black-search-icon.svg" alt="Search Icon">
+      <input type="text" placeholder="SEARCH">
+    </div>`;
+    navWrapper.insertAdjacentHTML('beforeend', navSearchInputHTML);
+    const searchInputWrapper = document.querySelector('.search-input-wrapper');
+    const searchIcon = document.querySelector('.nav-brand .default-content-wrapper .icon-white-search-icon');
+    searchIcon.addEventListener('click', (() => {
+      if (searchInputWrapper.classList.contains('active')) {
+        searchInputWrapper.classList.remove('active');
+      } else {
+        searchInputWrapper.classList.add('active');
+      }
+    }));
+  } else {
+    const searchWrapper = document.querySelector('.nav-sections .default-content-wrapper p');
+    const inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'text');
+    inputElement.setAttribute('placeholder', 'SEARCH');
+    searchWrapper.appendChild(inputElement);
+  } 
 }
 
 const bodyElements = document.getElementsByTagName('body');
