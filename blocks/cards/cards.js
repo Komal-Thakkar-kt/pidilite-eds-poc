9de60a556b7e1e0311021318fe8cc3e8b2e2f221
yashteli <yash.teli@teknopoint.in>
2025-04-06 14:43:40 +0530

    header-cipy js file build error solved

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -7,7 +7,7 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
-  
+
     // Add card classes
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
--- blocks/header-cipy/header-cipy.js
+++ blocks/header-cipy/header-cipy.js
@@ -1,18 +1,17 @@
-import { loadHeader } from "../../scripts/aem";
+import { loadHeader } from '../../scripts/aem.js';
 
 /**
  * Loads everything that doesn't need to be delayed.
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-    const main = doc.querySelector('main');
-
-    loadHeader(doc.querySelector('header-cipy'));
+//   const main = doc.querySelector('main');
+  loadHeader(doc.querySelector('header-cipy'));
 }
 
 async function loadPage() {
-    await loadLazy(document);
-    // loadDelayed();
+  await loadLazy(document);
+  // loadDelayed();
 }
 
 loadPage();
