3dd65e008e90e019f6b2db865b96e5601b0368ef
yashteli <yash.teli@teknopoint.in>
2025-04-09 05:49:08 +0530

    local file enabled for swiper

--- blocks/hero-carousel/hero-carousel.js
+++ blocks/hero-carousel/hero-carousel.js
@@ -1,5 +1,5 @@
-// import Swiper from './swiper-bundle.min.js';
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
+import Swiper from './swiper-bundle.min.js';
+// import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
 export default function decorateCarousel(block) {
   block.classList.add('swiper');
